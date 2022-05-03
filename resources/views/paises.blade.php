716435fe3afe29d1c8f07cfc0ba9110a8f679819
maleja <maleja@ejemplo.com>
2022-05-03 11:16:58 -0500

    primeras migraciones

--- resources/views/paises.blade.php
+++ resources/views/paises.blade.php
@@ -24,20 +24,20 @@
         <tbody>
             @foreach($paises as $pais => $infopais)
                 <tr>
-                    <td rowspan="{{count($infopais['ciudades'])}}">
+                    <td class="text-success" rowspan="{{count($infopais['ciudades'])}}" >
                         {{$pais}}
                     </td>
-                    <td rowspan="{{count($infopais['ciudades'])}}">
+                    <td class="text-warning" rowspan="{{count($infopais['ciudades'])}}">
                         {{$infopais["capital"]}}
                     </td>
-                    <td rowspan="{{count($infopais['ciudades'])}}">
+                    <td class="text-info" rowspan="{{count($infopais['ciudades'])}}">
                         {{$infopais["moneda"]}}
                     </td>
                     <td rowspan="{{count($infopais['ciudades'])}}">
                         {{$infopais["poblacion"]}}
                     </td>
                     @foreach($infopais["ciudades"] as $ciudad)
-                        <th> 
+                        <th class="p-3 mb-2 bg-success text-white" > 
                          {{ $ciudad }}   
                         </th>
                     </tr>
